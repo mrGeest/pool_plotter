473ceb1636cd3a9e9bbd4937ef104a39d19ee7a6
mrGeest <git@vandergee.st>
2020-05-23 12:34:42 -0700

    Cleanin up folder paths, step 2.

--- path_config.py
+++
@@ -1,9 +0,0 @@
-# Store the location of the current file so that we can specify a relative path that will always work
-# (A crontab runs the script from ~, for example)
-import os
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-data_folder = os.path.join(__location__, '../pool_logger_data/')
-
-# This is where the files from the raspberry pi come in:
-upload_folder = '/upload_folder/'
-
