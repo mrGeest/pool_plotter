798b5e45c91b7f9fbb296d148fb4602e902f28ca
mrGeest <git@vandergee.st>
2020-05-23 12:45:41 -0700

    Further folder cleanup.

--- pool_plots_caller_day.py
+++ pool_plots_caller_day.py
@@ -26,13 +26,14 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 try:
     # path_config will exist on the server
     import path_config    
-    target_folder = path_config.data_folder
     upload_folder = path_config.upload_folder
-    
+    archive_folder = path_config.data_folder
+    web_server_folder = path_config.web_server_folder
 except:
     # Probably debug running locally
     upload_folder = None
-    data_folder = '../pool_logger_data/'
+    archive_folder = '../pool_logger_data/'
+    web_server_folder = None
     
 
 file_prefix = 'pool_' # May be empty to copy any file
@@ -45,7 +46,7 @@ if upload_folder is not None:
             # this file is not our problem
             continue
         
-        copyfile(upload_folder + file, target_folder + file)
+        copyfile(upload_folder + file, archive_folder + file)
         
         # Maybe also delete this file?
     
@@ -55,9 +56,10 @@ import pool_plots_basic_day
 
 water_temp_file, sunambient_temp_file, enclosure_temp_file, cpu_temp_file = pool_plots_basic_day.make_plots()
 
-copyfile(os.path.join(__location__, cpu_temp_file), "/var/www/html/" + cpu_temp_file)
-copyfile(os.path.join(__location__, water_temp_file), "/var/www/html/" + water_temp_file)
-copyfile(os.path.join(__location__, sunambient_temp_file), "/var/www/html/" + sunambient_temp_file)
-copyfile(os.path.join(__location__, enclosure_temp_file), "/var/www/html/" + enclosure_temp_file)
+if web_server_folder is not None:
+    copyfile(os.path.join(__location__, cpu_temp_file), web_server_folder + cpu_temp_file)
+    copyfile(os.path.join(__location__, water_temp_file), web_server_folder + water_temp_file)
+    copyfile(os.path.join(__location__, sunambient_temp_file), web_server_folder + sunambient_temp_file)
+    copyfile(os.path.join(__location__, enclosure_temp_file), web_server_folder + enclosure_temp_file)
 
 
--- pool_plots_caller_week.py
+++ pool_plots_caller_week.py
@@ -21,15 +21,25 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 
 # Actually, don't and rely on the 24 hour plot to have done that for us
    
+try:
+    # path_config will exist on the server
+    import path_config    
+    web_server_folder = path_config.web_server_folder
+except:
+    # Probably debug running locally
+    web_server_folder = None
+    
+
 #%% ------ Step 2, call the various sub scripts and have them make their plots ----
 
 import pool_plots_basic_week
 
 water_temp_file, sunambient_temp_file, enclosure_temp_file, cpu_temp_file = pool_plots_basic_week.make_plots()
 
-copyfile(os.path.join(__location__, cpu_temp_file), "/var/www/html/" + cpu_temp_file)
-copyfile(os.path.join(__location__, water_temp_file), "/var/www/html/" + water_temp_file)
-copyfile(os.path.join(__location__, sunambient_temp_file), "/var/www/html/" + sunambient_temp_file)
-copyfile(os.path.join(__location__, enclosure_temp_file), "/var/www/html/" + enclosure_temp_file)
+if web_server_folder is not None:
+    copyfile(os.path.join(__location__, cpu_temp_file), web_server_folder + cpu_temp_file)
+    copyfile(os.path.join(__location__, water_temp_file), web_server_folder + water_temp_file)
+    copyfile(os.path.join(__location__, sunambient_temp_file), web_server_folder + sunambient_temp_file)
+    copyfile(os.path.join(__location__, enclosure_temp_file), web_server_folder + enclosure_temp_file)
 
 
